222f035910ce09b18a89a000a89ce49c4a91be25
U-BSD\301968 <301968@SHS-10D8A7CD2.beaverton.k12.or.us>
2016-09-15 13:32:51 -0700

    Added comments

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -1,3 +1,6 @@
+//C++ Project 1 - Nathan Purwosumarto - 9/15/16
+//A program that generates a random number and prompts the user to guess the number.
+
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
@@ -15,6 +18,7 @@ int main()
   char endresult;
   cout << "You have found the RNG Guessing Game" << endl << endl;
   while (running == true and gameover == false) {
+    //Prompts the user for an integer
       cout << "Please input your guess." << endl;
       guesses += 1;
       cin >> input;
@@ -29,6 +33,7 @@ int main()
 	cout << "Number of guesses:" << guesses << endl;
 	cout << "Do you want to quit? (y/n)" << endl;
 	cin >> endresult;
+	//If the user types in 'y', then the program terminates
 	if(endresult == 'y') {
 	  gameover = true;
 	}
